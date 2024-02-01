799e0fee20eb62d48e2495a5e13b86c4017e0971
EzzEl-DeenMohamed <Ezzasdezz123@gmail.com>
2024-02-01 18:54:50 +0200

    ~

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const Labeler = require("../models/Labeler");
 const QC = require("../models/Qc");
 const TL = require("../models/Tl");
 const STL = require("../models/Stl");
+const Info = require("../models/Info");
 const bcrypt = require("bcryptjs");
 const flash = require("connect-flash");
 
@@ -17,50 +18,55 @@ exports.postLogin = async (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const user =
-      (await Labeler.findOne({ username: username }).populate('info')) ||
-      (await QC.findOne({ 'info.username': username }).populate('info')) ||
-      (await TL.findOne({ 'info.username': username }).populate('info')) ||
-      (await STL.findOne({ 'info.username': username }).populate('info'));
+    const info = await Info.findOne({ username: username });
 
-    if (user) {
-      const match = await bcrypt.compare(password, user.info.password);
-
-      if (match) {
-        req.session.isLoggedin = true;
-        req.session.user = user;
-
-        switch (user.info.position) {
-          case "Labeler":
-            res.redirect("/labeler/home");
-            break;
-          case "Quality Control":
-            res.redirect("/qc/home");
-            break;
-          case "Team Lead":
-            res.redirect("/tl/home");
-            break;
-          case "Senior Team Lead":
-            res.redirect("/stl/home");
-            break;
-          default:
-            res.redirect("/");
-        }
-      } else {
-        // Incorrect password
-        req.flash("error", "Incorrect password");
-        res.redirect("/login");
-      }
-    } else {
+    if (!info) {
       // User not found
       req.flash("error", "User not found");
-      res.redirect("/login");
+      return res.redirect("/login");
+    }
+      // ||
+      // await QC.findOne({ info: info }) ||
+      // await TL.findOne({ info: info }) ||
+      // await STL.findOne({ info: info })
+    const user = await Labeler.findOne({ info: info });
+    
+    if (!user) {
+      // User not found
+      req.flash("error", "User not found");
+      return res.redirect("/login");
+    }
+
+    const match = await bcrypt.compare(password, info.password);
+    console.log(match);
+    if (match) {
+      req.session.isLoggedin = true;
+      req.session.user = user;
+
+      switch (info.position) {
+        case "Labeler":
+          return res.redirect("/labeler/home");
+        case "Quality Control":
+          return res.redirect("/qc/home");
+        case "Team Lead":
+          return res.redirect("/tl/home");
+        case "Senior Team Lead":
+          return res.redirect("/stl/home");
+        default:
+          return res.redirect("/");
+      }
+    } else {
+      // Incorrect password
+      req.flash("error", "Incorrect password");
+      return res.redirect("/login");
     }
   } catch (err) {
-    console.error(err);
-    res.redirect("/login");
+    console.error("Error during login:", err);
+    req.flash("error", "An unexpected error occurred");
+    return res.redirect("/login");
   }
 };
+
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
